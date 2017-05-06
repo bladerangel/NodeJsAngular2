8c5b777384bb0cd45e6279a06a723ca3234df469
bladerangel <bladerangel10@gmail.com>
2017-05-06 20:12:22 -0300

    add devdependencies nodemon

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 'use strict';
 
 let express = require('express');
+let bodyParser = require('body-parser');
+
 let app = express();
+let port = process.env.PORT || 3678;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.listen(3678, function () {
-    console.log('Api rest work in http://localhost:3678');
+    console.log(`Api rest work in http://localhost:${port}`);
 });
