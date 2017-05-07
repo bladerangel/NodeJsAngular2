f224ce9c11a5e2b8f735dcb43b7870199b7f78fa
bladerangel <bladerangel10@gmail.com>
2017-05-07 00:58:34 -0300

    refactoring index

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ let app = require('./app');
 let port = process.env.PORT || 3678;
 let mongoose = require('mongoose');
 
-
-
 mongoose.connect('mongodb://localhost:27017/favoritedb', (err, res) => {
     if (err) {
         throw err;
     } else {
-        console.log('Connected successfully!')
+        console.log('Connected successfully');
         app.listen(3678, () => {
             console.log(`Api rest work in http://localhost:${port}`);
         });
