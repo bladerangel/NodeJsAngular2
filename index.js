f9e3128527289f83028c8b94e795e7e8494fcd3c
bladerangel <bladerangel10@gmail.com>
2017-05-07 00:38:09 -0300

    connect mogoose

--- index.js
+++ index.js
@@ -2,7 +2,19 @@
 
 let app = require('./app');
 let port = process.env.PORT || 3678;
+let mongoose = require('mongoose');
 
-app.listen(3678, () => {
-    console.log(`Api rest work in http://localhost:${port}`);
+
+
+mongoose.connect('mongodb://localhost:27017/favoritedb', (err, res) => {
+    if (err) {
+        throw err;
+    } else {
+        console.log('Connected successfully!')
+        app.listen(3678, () => {
+            console.log(`Api rest work in http://localhost:${port}`);
+        });
+    }
 });
+
+
