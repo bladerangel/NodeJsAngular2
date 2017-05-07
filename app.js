71b9fc6886806554aa907593f565f957cc00c2e6
bladerangel <bladerangel10@gmail.com>
2017-05-07 19:51:18 -0300

    config http cors

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ let api = require('./routes/favorite');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+        'X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method');
+    res.header('Access-Control-Request-Methods', 'GET, POST, PUT, DELETE ,OPTIONS');
+    res.header('Allow', 'GET, POST, PUT, DELETE ,OPTIONS');
+
+    next();
+});
+
 app.use('/api', api);
 
 module.exports = app;
