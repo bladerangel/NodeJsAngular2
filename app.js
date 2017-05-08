f0d38d7ff40b3c4da3196883e542c3d1335fbb2b
bladerangel <bladerangel10@gmail.com>
2017-05-08 17:45:43 -0300

    change cors

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers',
         'X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method');
-    res.header('Access-Control-Request-Methods', 'GET, POST, PUT, DELETE ,OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE ,OPTIONS');
     res.header('Allow', 'GET, POST, PUT, DELETE ,OPTIONS');
 
     next();
